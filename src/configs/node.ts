82d7c07f25d06f5df6793c85aa68dd846ce45af9
pnodet <paul.nodet@gmail.com>
2024-02-09 00:49:07 +0800

    fix: stricter node config

--- src/configs/node.ts
+++ src/configs/node.ts
@@ -13,7 +13,17 @@ export const node = (): FlatESLintConfig[] => {
 		{
 			files: [GLOB_SRC],
 			rules: {
-				'n/file-extension-in-import': ['off'],
+				'n/file-extension-in-import': [
+					'error',
+					'always',
+					{
+						// TypeScript doesn't yet support using extensions and fails with error TS2691.
+						'.ts': 'never',
+						'.tsx': 'never',
+						'.mts': 'never',
+						'.cts': 'never',
+					},
+				],
 				'n/handle-callback-err': ['error', '^(err|error)$'],
 				'n/no-deprecated-api': ['error'],
 				'n/no-exports-assign': 'error',
@@ -25,7 +35,7 @@ export const node = (): FlatESLintConfig[] => {
 				'n/no-unpublished-bin': ['error'],
 				'n/prefer-global/buffer': ['off'],
 				'n/prefer-global/console': ['error', 'always'],
-				'n/prefer-global/process': ['off'],
+				'n/prefer-global/process': ['error', 'always'],
 				'n/prefer-global/text-decoder': ['error', 'always'],
 				'n/prefer-global/text-encoder': ['error', 'always'],
 				'n/prefer-global/url': ['error', 'always'],
