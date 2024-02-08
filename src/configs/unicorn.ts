cfd427b05a934f25fea4c5a8acfde7b5a8e678be
pnodet <paul.nodet@gmail.com>
2024-02-09 00:38:45 +0800

    feat: add unicorn/prevent-abbreviation rule

--- src/configs/unicorn.ts
+++ src/configs/unicorn.ts
@@ -2,6 +2,7 @@ import pluginUnicorn from 'eslint-plugin-unicorn';
 import {GLOB_SRC} from '../globs';
 import type {FlatESLintConfig} from 'eslint-define-config';
 
+// eslint-disable-next-line max-lines-per-function
 export const unicorn = (): FlatESLintConfig[] => {
 	return [
 		{
@@ -105,6 +106,110 @@ export const unicorn = (): FlatESLintConfig[] => {
 				'unicorn/prefer-ternary': ['error', 'only-single-line'],
 				'unicorn/prefer-top-level-await': 'error',
 				'unicorn/prefer-type-error': 'error',
+				// https://github.com/xojs/xo/blob/main/config/plugins.cjs#L38
+				'unicorn/prevent-abbreviations': [
+					'error',
+					{
+						checkFilenames: false,
+						checkDefaultAndNamespaceImports: false,
+						checkShorthandImports: false,
+						extendDefaultReplacements: true,
+						replacements: {
+							// https://thenextweb.com/dd/2020/07/13/linux-kernel-will-no-longer-use-terms-blacklist-and-slave/
+							whitelist: {
+								include: true,
+							},
+							blacklist: {
+								exclude: true,
+							},
+							master: {
+								main: true,
+							},
+							slave: {
+								secondary: true,
+							},
+
+							// Reverse.
+							application: {
+								app: true,
+							},
+							applications: {
+								apps: true,
+							},
+
+							// Disable some that may be too annoying.
+							env: false,
+							i: false, // Do it at some point, but not ready for it yet. Maybe 2025.
+
+							// Not part of `eslint-plugin-unicorn`. Upstream them at some point.
+							bin: {
+								binary: true,
+							},
+							eof: {
+								endOfFile: true,
+							},
+							impl: {
+								implement: true,
+								implementation: true,
+							},
+							anim: {
+								animation: true,
+							},
+							calc: {
+								calculate: true,
+							},
+							dict: {
+								dictionary: true,
+							},
+							dup: {
+								duplicate: true,
+							},
+							enc: {
+								encode: true,
+								encryption: true,
+							},
+							gen: {
+								generate: true,
+								general: true,
+							},
+							gfx: {
+								graphics: true,
+							},
+							inc: {
+								increment: true,
+							},
+							iter: {
+								iterate: true,
+								iterator: true,
+							},
+							nav: {
+								navigate: true,
+								navigation: true,
+							},
+							norm: {
+								normalize: true,
+							},
+							notif: {
+								notification: true,
+							},
+							perf: {
+								performance: true,
+							},
+							proc: {
+								process: true,
+							},
+							rand: {
+								random: true,
+							},
+							sys: {
+								system: true,
+							},
+							temp: {
+								temporary: true,
+							},
+						},
+					},
+				],
 				'unicorn/relative-url-style': 'error',
 				'unicorn/require-array-join-separator': 'error',
 				'unicorn/require-number-to-fixed-digits-argument': 'error',
