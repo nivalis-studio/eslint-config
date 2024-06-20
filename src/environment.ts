a54b87a65a7469df39cc263b0956a4e231e915a5
pnodet <paul.nodet@gmail.com>
2024-06-20 15:23:31 +0200

    feat: add nvim is in editor

--- src/environment.ts
+++ src/environment.ts
@@ -3,6 +3,7 @@ import { isPackageExists } from 'local-pkg';
 export const IN_IS_EDITOR = !!(
   (process.env.VSCODE_PID ||
     process.env.VSCODE_CWD ||
+    process.env.NVIM ||
     process.env.JETBRAINS_IDE ||
     process.env.VIM) &&
   !process.env.CI
