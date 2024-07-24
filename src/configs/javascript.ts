f31c32c11096b5bf8d8a57a95921795b2d81d81f
pnodet <paul.nodet@gmail.com>
2024-07-24 14:11:46 +0200

    feat: add commonjs config

--- src/configs/javascript.ts
+++ src/configs/javascript.ts
@@ -36,6 +36,27 @@ export const javascript = (): TypedFlatConfigItem[] => {
       },
     },
     {
+      name: 'nivalis/commonjs/config',
+      files: ['**/*.cjs'],
+      languageOptions: {
+        ecmaVersion: 2022,
+        globals: {
+          ...globals.browser,
+          ...globals.es2021,
+          ...globals.node,
+          ...globals.commonjs,
+          document: 'readonly',
+          navigator: 'readonly',
+          window: 'readonly',
+        },
+        parserOptions: {
+          ecmaVersion: 2022,
+          sourceType: 'script',
+        },
+        sourceType: 'script',
+      },
+    },
+    {
       files: [GLOB_SRC],
       name: 'nivalis/javascript/rules',
       rules: {
