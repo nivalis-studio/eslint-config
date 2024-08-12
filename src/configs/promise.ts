e4c3afa3d615529f0a0ab5b8c39b5344f6eeb746
pnodet <paul.nodet@gmail.com>
2024-08-12 15:06:09 +0200

    feat: update promise rules

--- src/configs/promise.ts
+++ src/configs/promise.ts
@@ -7,5 +7,17 @@ export const promise = (): TypedFlatConfigItem[] => {
       ...(pluginPromise.configs['flat/recommended'] as TypedFlatConfigItem),
       name: 'nivalis/promise/setup',
     },
+    {
+      name: 'nivalis/promise/rules',
+      rules: {
+        'promise/always-return': [
+          'error',
+          {
+            ignoreLastCallback: true,
+          },
+        ],
+        'promise/no-multiple-resolved': 'warn',
+      },
+    },
   ];
 };
