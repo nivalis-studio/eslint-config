9fbe2104b0b834483b78e7f62792bee330f5ea3e
pnodet <paul.nodet@gmail.com>
2024-10-04 17:42:37 +0200

    feat(unicorn): disable rule global this

--- src/configs/unicorn.ts
+++ src/configs/unicorn.ts
@@ -14,6 +14,7 @@ export const unicorn = (): Linter.Config[] => {
             ignore: [/^[A-Z]+\..*$/],
           },
         ],
+        'unicorn/prefer-global-this': 'off',
         'unicorn/prefer-string-raw': 'off',
         'unicorn/prevent-abbreviations': 'off',
         'unicorn/no-array-callback-reference': 'warn',
